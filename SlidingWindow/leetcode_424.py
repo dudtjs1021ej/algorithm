34b9930670e66b8f667d5fc7adc0c9790f76d696
youngsun <dudtjs1021@hansung.ac.kr>
2022-01-16 00:03:13 +0900

    [add] SlidingWindow/leetcode_424.py

---
+++ SlidingWindow/leetcode_424.py
@@ -0,0 +1,17 @@
+import collections
+# s = "AAABBC" k = 2 일 때, k번 바꿔서 연속으로 똑같은 문자 최대 개수 구하기
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = right = 0
+        counts = collections.Counter()
+        for right in range(1, len(s) + 1):
+            counts[s[right - 1]] += 1
+            # 가장 많이 등장하는 문자 탐색
+            max_char_n = counts.most_common(1)[0][1]
+
+            # k 초과시 왼쪽 포인터 이동
+            if right - left - max_char_n > k:
+                counts[s[left]] -= 1
+                left += 1
+        return right - left
